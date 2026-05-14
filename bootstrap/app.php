068e45eb801cca3721d496716a1c2cd375135a33
Andrés <acorrecham@ucentral.edu.co>
2026-05-13 23:16:40 -0500

    Creación de archivos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,14 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
+    
+	$middleware->trustProxies{
+		'*',
+		Request::HEADER_X_FORWARDED_FOR |
+			Request::HEADER_X_FORWARDED_HOST |
+			Request::HEADER_X_FORWARDED_PORT |
+			Request::HEADER_X_FORWARDED_PROTO
+	};	
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
