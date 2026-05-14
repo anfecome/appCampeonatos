6b221d8e0561525724bb6cdecf1837eb9a799d03
Andrés <acorrecham@ucentral.edu.co>
2026-05-13 23:40:51 -0500

    Edición

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,15 +22,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
-        ]);
-    
-	$middleware->trustProxies{
-		'*',
-		Request::HEADER_X_FORWARDED_FOR |
-			Request::HEADER_X_FORWARDED_HOST |
-			Request::HEADER_X_FORWARDED_PORT |
-			Request::HEADER_X_FORWARDED_PROTO
-	};	
+        ]);    
+		
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
